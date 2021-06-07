a0386f305e8d95f2e363326048308a43f8d14aa0
Ritika Jain <ritika192713@gmail.com>
2021-06-07 12:15:19 +0530

    update commit

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const cons = require('consolidate');
+const https = require("https");
+const path = require("path");
+const static_path = path.join(__dirname, "./views");
 const app = express();
-const port = process.env.PORT || 5000;
-app.use(express.static('public'));
+const PORT = process.env.PORT || 5000;
 
-//make way for some custom css, js and images
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
+app.use(express.static(static_path));
 app.engine('html', cons.swig)
 app.set('view engine', 'html');
+app.set('views', static_path);
 
 app.get("/", (req, res) => {
     res.render("index");
-});
+  });
 
-app.listen(port, () => {
-    console.log(`Server is running at ${port} `);
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT} `);
 })
