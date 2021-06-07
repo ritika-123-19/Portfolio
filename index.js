444ddcda3d760f2c2b23b0b1d76e4c5602f0b9ad
Ritika Jain <ritika192713@gmail.com>
2021-06-07 16:35:19 +0530

    update commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var express = require("express");
- 
-var app = express();
- 
-app.use(express.static('views'));
- 
-//make way for some custom css, js and images
-app.use('/css', express.static(__dirname + '/views/css'));
-app.use('/js', express.static(__dirname + '/views/js'));
-app.use('/images', express.static(__dirname + '/views/images'));
- 
-var server = app.listen(8081, function(){
-    var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
-});
+// create an express app
+const express = require("express")
+const app = express()
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("index")
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
