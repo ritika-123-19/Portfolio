902132579ff4fe180467a3eb17aa3127ae5fb33f
Ritika Jain <ritika192713@gmail.com>
2021-06-07 11:18:31 +0530

    update commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var app = express();
-
+const port = process.env.PORT || 5000;
 app.use(express.static('public'));
 
 //make way for some custom css, js and images
@@ -9,7 +9,6 @@ app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
 
-var server = app.listen(8081, function(){
-    var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
-});
+app.listen(port, () => {
+    console.log(`Server is running at ${port} `);
+})
