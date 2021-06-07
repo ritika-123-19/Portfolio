8a748b5eb39cf77c49ccdf0d44f22e5ba63f9762
Ritika Jain <ritika192713@gmail.com>
2021-06-07 11:20:13 +0530

    update commit

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
 
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
 app.listen(port, () => {
     console.log(`Server is running at ${port} `);
 })
