69bf23531bba57cc337469ff2e14e32ab5ec09fb
Ritika Jain <ritika192713@gmail.com>
2021-06-07 16:24:54 +0530

    update commit

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require("express");
  
 var app = express();
  
-app.use(express.static('public'));
+app.use(express.static('views'));
  
 //make way for some custom css, js and images
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/css', express.static(__dirname + '/views/css'));
+app.use('/js', express.static(__dirname + '/views/js'));
+app.use('/images', express.static(__dirname + '/views/images'));
  
 var server = app.listen(8081, function(){
     var port = server.address().port;
